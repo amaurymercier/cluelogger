6a0cd9ad5fd5f6c8bbb21536e1a0512f08cd8aee
Amaury Mercier <amaury.m@hellowatt.fr>
2022-01-05 14:04:04 +0100

    wip

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ClueLoggerTabContainer from './navigation/Navigation.js';
+import ClueLoggerTabContainer from './navigation/Navigation';
 
 import moment from 'moment/min/moment-with-locales';
 
 moment.locale('fr');
 
+// noinspection JSUnusedGlobalSymbols
 export default function App() {
-  // StatusBar.setStatusBarStyle('light-content', true);
   return (
     <SafeAreaProvider>
       <ClueLoggerTabContainer />
